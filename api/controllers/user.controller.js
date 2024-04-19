cc27cec3e0a595d234b641f7bf806dc20f8bff80
Mohammed Saleh <mdsaleh23@gmail.com>
2024-04-19 15:27:16 +0530

    Added delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,3 +52,16 @@ export const updateUser = async (req, res, next) => {
             next(error)
         }
 }
+
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.userId){
+        return next(errorHandler(403, "You are not authorized to delete this user"))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId)
+        res.status(200).json("User has been deleted successfully!")
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { test } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
 router.get('/test', test)
 router.put('/update/:userId', verifyUser, updateUser)
+router.delete('/delete/:userId', verifyUser, deleteUser)
 
 export default router
