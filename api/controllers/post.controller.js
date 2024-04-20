f692f3331a166a37e18aa6490181309b9138629c
Mohammed Saleh <mdsaleh23@gmail.com>
2024-04-20 20:44:17 +0530

    created get the all post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -19,3 +19,58 @@ export const create = async (req, res, next) => {
         next(error)
     }
 }
+
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0 // here parseInt is used to convert the string to integer 
+        const limit = parseInt(req.query.limit) || 9
+        const sortDirection = req.query.order === "asc" ? 1 : -1
+        const posts = await Post.find({
+            ...(req.query.userId  && { userId: req.query.userId }), // here this is using for filtering the posts by userId
+            ...(req.query.category && { category: req.query.category }), // here this is using for filtering the posts by category
+            ...(req.query.slug && { slug: req.query.slug }), // here this is using for filtering the posts by slug
+            ...(req.query.postId && { _id: req.query.postId }), // here this is using for filtering the posts by postId
+
+            ...(req.query.searchTerm && { // here this is using for searching the posts by searchTerm with both title and content
+                $or: [ // https://www.mongodb.com/docs/manual/reference/operator/query/or/
+                    {
+                        title: {
+                            $regex: req.query.searchTerm,  // Provides regular expression capabilities for pattern matching strings in queries.  https://www.mongodb.com/docs/manual/reference/operator/query/regex/#mongodb-query-op.-regex
+
+                            $options: "i"   // case-insensitive
+                        }
+                    },
+                    {
+                        content: {
+                            $regex: req.query.searchTerm, 
+                            $options: "i"   // case-insensitive
+                        }
+                    }
+                ]
+            }) 
+    }).sort({ updatedAt: sortDirection}).skip(startIndex).limit(limit)
+
+    const totalPost = await Post.countDocuments() // here this is used to count the total number of posts
+    
+    const now = new Date()
+    const oneMonthAgo = new Date( // here this is used to get the date of one month ago from the current date
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate()
+    )
+
+    const lastMonthPosts = await Post.countDocuments({
+        createdAt: { $gte: oneMonthAgo } // here this is used to get the posts which are updated in last month
+    })
+
+    res.status(200).json({
+        posts,
+        totalPost,
+        lastMonthPosts
+    })
+
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, create)
+router.get("/getposts", getPosts)
 
 export default router;
