1d699979f793411e7467a042f51177ce699a4b26
Mohammed Saleh <mdsaleh23@gmail.com>
2024-04-20 17:33:18 +0530

    Completed upload post image functionality:

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,7 +7,7 @@ const router = Router()
 
 router.get('/test', test)
 router.put('/update/:userId', verifyUser, updateUser)  // here userId is related to the sign in mongodb id of the user and both are same
-router.delete('/delete/:userId', verifyUser, deleteUser) // here userId is related to the sign in mongodb id of the user 
+router.delete('/delete/:userId', verifyUser, deleteUser) // here userId is related to the sign in mongodb id of the user
 router.post('/signout', signOut)
 
 export default router
