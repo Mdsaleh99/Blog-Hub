cd11a166b621e4320baf048f0a73454e28683e0c
Mohammed Saleh <mdsaleh23@gmail.com>
2024-04-23 00:50:58 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Blog Hub
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Blog Hub! Blog Hub is a platform where you can read
+              articles on various topics such as Full stack development,
+              Programming languages, Software engineering, etc.
+            </p>
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About
