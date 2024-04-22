e9985024f58cc85ce7d87c884c5e8acebe54692e
Mohammed Saleh <mdsaleh23@gmail.com>
2024-04-22 22:54:39 +0530

    completed home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -31,75 +31,76 @@ function Header() {
   }
 
   return (
-    <Navbar className='border-b-2'>
-      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 rounded-lg via-purple-500 to-pink-500 text-white' >Blog</span>
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 rounded-lg via-purple-500 to-pink-500 text-white">
+          Blog
+        </span>
         Hub
       </Link>
       <form action="">
         <TextInput
-          type='text'
-          placeholder='search...'
+          type="text"
+          placeholder="search..."
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className="hidden lg:inline"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
-          
+      <div className="flex gap-2 md:order-2">
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
-          <Dropdown arrowIcon={false} inline label={
-            <Avatar
-              alt='avatar'
-              img={currentUser.profilePicture}
-              rounded
-            />
-          }>
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="avatar" img={currentUser.profilePicture} rounded />
+            }
+          >
             <Dropdown.Header>
-              <span className='block text-sm'>@{currentUser.userName}</span>
-              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+              <span className="block text-sm">@{currentUser.userName}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
-              <Dropdown.Item>
-                Profile
-              </Dropdown.Item>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item onClick={handleSignOut}>
-                Sign out
-              </Dropdown.Item>
+              <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
             </Link>
           </Dropdown>
         ) : (
-          <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline >
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
               Sign In
-          </Button>
-        </Link>
+            </Button>
+          </Link>
         )}
-        
+
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={"div"}>
-            <Link to='/'>
-              Home
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/about"} as={"div"}>
-            <Link to='/about'>
-              About
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
-      
-
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
 
 export default Header
--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function PostCard({post}) {
   return (
-    <div className='group relative w-full border h-[400px] overflow-hidden rounded-lg sm:w-[330px] border-teal-500 hover:border-2 transition-all  '>
+    <div className='group relative w-full border h-[400px] overflow-hidden rounded-lg sm:w-[340px] border-teal-500 hover:border-2 transition-all  '>
         <Link to={`/post/${post.slug}`}>
-            <img src={post.image} alt="post cover" className='h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20' />
+            <img src={post.image} alt="post cover" className='h-[220px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20' />
         </Link>
         <div className='p-3 flex flex-col gap-2 '>
             <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
