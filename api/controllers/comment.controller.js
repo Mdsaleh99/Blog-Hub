4deb21066cdee78c2c1ba357dee7b71f0952cda4
Mohammed Saleh <mdsaleh23@gmail.com>
2024-04-22 12:08:22 +0530

    added like functionality to the comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -33,3 +33,80 @@ export const getPostComments = async (req, res, next) => {
     }
     
 }
+
+export const likeComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId)
+        if(!comment){
+            return next(errorHandler(404, "Comment not found"))
+        }
+        const userIndex = comment.likes.indexOf(req.user.id)
+        if(userIndex === -1){
+            comment.numberOfLikes += 1
+            comment.likes.push(req.user.id)
+        }
+        else{
+            comment.numberOfLikes -= 1
+            comment.likes.splice(userIndex, 1)
+        }
+        await comment.save()
+        res.status(200).json(comment)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+/**
+ * This code snippet checks if the req.user.id exists in the likes array of a comment object. If it doesn't exist, it adds the req.user.id to the likes array.
+
+Here's a breakdown of how it works:
+
+comment.likes: This refers to an array (likes) stored within the comment object.
+indexOf(req.user.id): This method checks if req.user.id exists in the likes array. It returns the index of the first occurrence of req.user.id in the array. If the element is not found, it returns -1.
+userIndex: This variable stores the index of req.user.id in the likes array.
+if(userIndex === -1): This condition checks if req.user.id is not found in the likes array.
+comment.likes.push(req.user.id): If req.user.id is not found (userIndex === -1), it means the user has not liked the comment yet. So, the req.user.id is added to the likes array using the push() method.
+ * 
+ * 
+ * 
+ * comment.likes: This refers to an array (likes) stored within the comment object.
+splice(userIndex, 1): This is a call to the splice() method on the likes array.
+userIndex: This is the index of the element to remove from the array. userIndex specifies the position of the element to remove.
+1: This indicates the number of elements to remove from the array. In this case, only one element is being removed.
+So, comment.likes.splice(userIndex, 1) removes one element from the likes array at the specified userIndex position, effectively deleting an item from the list of likes associated with the comment object. After this operation, the likes array will be modified accordingly, with the specified element removed.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * The splice() method in JavaScript is used to change the contents of an array by removing or replacing existing elements and/or adding new elements in place. It modifies the original array and returns an array containing the removed elements, if any.
+
+Here's how splice() works:
+
+Syntax: The syntax for the splice() method is as follows:
+
+array.splice(startIndex, deleteCount, item1, item2, ...);
+
+Parameters:
+startIndex: The index at which to start changing the array. If negative, it will begin that many elements from the end of the array (with -1 indicating the last element of the array).
+deleteCount (optional): The number of elements to remove from the array. If omitted or if greater than the number of elements remaining from startIndex to the end of the array, all elements from startIndex to the end of the array will be deleted.
+item1, item2, ... (optional): Elements to add to the array, beginning at the startIndex.
+Return Value: The splice() method returns an array containing the removed elements, if any. If no elements are removed, it returns an empty array.
+Mutability: The original array is modified by splice().
+
+ */
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { createComment, getPostComments } from '../controllers/comment.controller.js';
+import {
+  createComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.controller.js";
 import { verifyUser } from '../utils/verifyUser.js';
 
 
@@ -8,5 +12,6 @@ const router = express.Router();
 
 router.post('/create', verifyUser, createComment)
 router.get('/getpostcomments/:postId', getPostComments)
+router.put('/likecomment/:commentId', verifyUser, likeComment)
 
 export default router;
